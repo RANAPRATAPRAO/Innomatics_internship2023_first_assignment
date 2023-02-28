c2ecf0bf6dc96513ed45c47be912a704f4d74736
RANA PRATAP RAO <97173893+RANAPRATAPRAO@users.noreply.github.com>
2023-02-28 11:30:08 +0530

    Add files via upload

---
+++ proj_2/app.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+st.title(":red[ Innomatics Reasearch  Lab Internship 2023 ( Data App)]")
+st.header("I create my first streamlit app ")
+st.subheader(":blue[with the help of KANAV BANSAL sir I create first streamlit app  ]")
+st.subheader("This is our first assignment project")
+
+st.snow()
+
+btn_click = st.button("Self Introduction!")
+
+if btn_click == True:
+    st.header("RANA PRATAP RAO ")
+    st.subheader("college: Jct college of engineering and technology")
+    st.subheader("Branch : Conputer science engineering")
+    st.balloons()
